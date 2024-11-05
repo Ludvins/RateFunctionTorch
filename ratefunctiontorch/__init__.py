a05add845dbc69fe35e9ba8f57ba2e1b14eb507a
Ludvins <theludvins@gmail.com>
2024-11-05 11:59:47 +0100

    Cummulant approximation example

--- ratefunctiontorch/__init__.py
+++ ratefunctiontorch/__init__.py
@@ -1,5 +1,5 @@
 from .cumulant import get_loss, eval_cumulant
 from .rate import rate_function, inverse_rate_function
-from .class_api import RateCumulant
+from .class_api import RateCumulant, OnlineCumulant
 
-__all__ = ["get_loss", "eval_cumulant", "rate_function", "inverse_rate_function", "RateCumulant"]
+__all__ = ["get_loss", "eval_cumulant", "rate_function", "inverse_rate_function", "RateCumulant", "OnlineCumulant"]
--- ratefunctiontorch/class_api.py
+++ ratefunctiontorch/class_api.py
@@ -1,4 +1,5 @@
 
+import torch
 import torch.nn as nn
 from .rate import rate_function_from_losses, inverse_rate_function_from_losses
 from .cumulant import eval_cumulant_from_losses, get_loss
@@ -95,6 +96,67 @@ class RateCumulant:
             verbose=self.verbose
         )
     
+    def compute_cumulants(self, evaluation_points):
+        """
+        Compute the cumulants at given evaluation points using precomputed losses.
+        
+        Parameters
+        ----------
+        evaluation_points : scalar, list, tuple or torch.Tensor
+            Points at which the cumulants will be computed.
+            
+        Returns
+        -------
+        torch.Tensor
+            The cumulants evaluated at each point in evaluation_points.
+        """
+        return eval_cumulant_from_losses(self.losses, evaluation_points)
+    
+    
+
+class OnlineCumulant:
+    def __init__(self, model, buffer_size, loss_fn=nn.CrossEntropyLoss(reduction="none"), verbose=False):
+        """
+        Initialize the RateFunctionAPI with a model and dataloader.
+        
+        Parameters
+        ----------
+        model : torch.nn.Module
+            The model to evaluate.
+        loader : torch.utils.data.DataLoader
+            The DataLoader providing data for evaluation.
+        loss_fn : callable, optional
+            The loss function used for evaluation. Default is nn.CrossEntropyLoss(reduction="none").
+        verbose : bool, optional
+            If True, display a progress bar. Default is False.
+        """
+        self.model = model
+        self.loss_fn = loss_fn
+        self.verbose = verbose
+        self.buffer_size = buffer_size
+
+        # Initialize the losses buffer
+        self.losses_weighted = torch.ones(buffer_size)
+        self.losses = torch.empty(0)
+        
+    def update_losses(self, inputs, targets):
+
+        # Set the model to evaluation mode (disables dropout, batch norm updates, etc.)
+        self.model.eval()
+                
+        logits = self.model(inputs)
+            
+        # Compute the loss between the predictions (logits) and the true labels
+        new_losses = self.loss_fn(logits, targets)
+        
+        self.losses = torch.cat([self.losses, new_losses])
+        if len(self.losses) > self.buffer_size:
+            self.losses = self.losses[-self.buffer_size:]
+        
+        # Scale the weight of old losses by 0.9
+        self.losses_weighted[:-len(new_losses)] *= 0.9
+
+
     def compute_cumulants(self, evaluation_points):
         """
         Compute the cumulants at given evaluation points using precomputed losses.
